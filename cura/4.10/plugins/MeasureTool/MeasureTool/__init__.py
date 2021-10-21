8397ab6616b3c059f9f04d94a1d447d4062cb214
Glutt0ne <59060622+Glutt0ne@users.noreply.github.com>
2021-10-21 16:44:28 -0700

    Establish all personal settings for cura

---
+++ cura/4.10/plugins/MeasureTool/MeasureTool/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2020 Aldo Hoeben / fieldOfView
+# MeasureTool is released under the terms of the AGPLv3 or higher.
+
+from . import MeasureTool
+
+from UM.i18n import i18nCatalog
+
+i18n_catalog = i18nCatalog("cura")
+
+
+def getMetaData():
+    return {
+        "tool": {
+            "name": i18n_catalog.i18nc("@label", "Measure"),
+            "description": i18n_catalog.i18nc(
+                "@info:tooltip", "Measure parts of objects."
+            ),
+            "icon": "resources/icons/tool_icon.svg",
+            "tool_panel": "resources/qml/MeasureTool.qml",
+            "weight": 6,
+        }
+    }
+
+
+def register(app):
+    return {"tool": MeasureTool.MeasureTool()}
